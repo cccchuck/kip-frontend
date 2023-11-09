4e897d6f0b1d7169262d1b70e65d589ec152d05b
Chuck <chuckgao0223@gmail.com>
2023-11-09 12:56:41 +0800

    feat: done

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,13 +1,11 @@
-const KIPQANFTAddress = '0xbcd14d2f893dd99f0774ac8113e94fd6232d6bb4'
-const KIPTokenAddress = '0x6a7a7b64c8714d46fa0e143a9dd0ca861d47e217'
-const KIPQueryAddress = '0x75d9912818697ef8716c518b4c4eadd73fbd79b4'
-const KIPAddress = '0xd76e1670c9251247bc8aab52ac9c9dd911b4b5c6'
+const KIPQANFTAddress = ''
+const KIPTokenAddress = ''
+const KIPQueryAddress = ''
+const KIPAddress = ''
 
-const sepoliaRPC =
-  'https://eth-sepolia.g.alchemy.com/v2/61_02oiFhc8CvMWwDnYsaCkNAuG7Ss8p'
+const sepoliaRPC = ''
 
-const mumbaiRPC =
-  'https://polygon-mumbai.g.alchemy.com/v2/FLiwGJM6jKJjyxB_lkSrFKjedNRSDgZ2'
+const mumbaiRPC = ''
 
 export {
   KIPQANFTAddress,
